a1ac302ca4f0fec272868f8048b369fb24b8d7a4
Brent Garner <bgarner@gmail.com>
2021-12-01 12:43:52 -0700

    add the video

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -13,6 +13,11 @@
 		<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl text-center">Welcome to Moroni's Quest</h1>
 		<p class="text-2xl mt-4 leading-6 font-medium text-gray-900 text-center">Calgary North Stake</p>
 		<p class="text-2xl mt-4 leading-6 font-medium text-gray-900 text-center">July 21 - 23, 2022</p>
+
+				<p class="text-center">
+<iframe style="margin: 20px auto;" width="560" height="315" src="https://www.youtube.com/embed/qW3j9fuWM1I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</p>
+		
 		<p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
 In the summer of 2022, Young Women and Young Men of the Calgary North Stake will experience first-hand some of the great adventures of living in the times of Nephi, Alma, and Moroni. Everyone will be assigned to a tribe, and together, we will journey in the wilderness, experience visions, follow prophets, and fight battles as we learn how to Come unto Christ.</p>
 		<p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
@@ -20,6 +25,7 @@ Youth will watch and participate in reenactments of important scenes from the Bo
 		<p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
 Our faith and testimonies of the Book of Mormon and the Saviour will be strengthened as we experience the Book of Mormon in a brand-new way together.
 		</p>
+
 
 
 	</div>

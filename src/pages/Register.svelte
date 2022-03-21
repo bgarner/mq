bb9baf33299f14e02961947bc1c71ad5705f4b22
Brent Garner <bgarner@gmail.com>
2022-03-21 17:16:44 -0600

    change registration deadline

--- src/pages/Register.svelte
+++ src/pages/Register.svelte
@@ -13,7 +13,7 @@
 		<h1 class="text-4xl tracking-tight font-bold text-gray-900 sm:text-4xl md:text-4xl lg:text-4xl xl:text-4xl text-center">Registration</h1>
 		<p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
 All Participants who will be attending Moroni's Quest will be required to fill out a registration form. This includes all adults that will be helping in any capacity.</p>
-		<p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">Registration will close on June 21, 2022.</p>
+		<p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">Registration will close on May 1, 2022.</p>
 		<p class="text-center mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl underline text-yellow-700"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdEyiU8noDXlkkzno3YS0lBa_PrmSUjwalz7Ta4GM-r7J7HzA/viewform?vc=0&c=0&w=1&flr=0" target="_blank">Open the Registration Form</a></p>
 
 	</div>
